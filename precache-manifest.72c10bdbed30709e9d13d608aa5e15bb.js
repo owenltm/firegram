9c1c647a15c426025bc45907a83432dd4bcbd318
owenltm <owenlwiantoro01@gmail.com>
2020-08-04 02:02:57 +0800

    Updates

---
+++ precache-manifest.72c10bdbed30709e9d13d608aa5e15bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06017961f282ef406c56c3e7f6702fce",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "f29eb190bb1ae8254c61",
+    "url": "/firegram/static/css/main.a94fd5ce.chunk.css"
+  },
+  {
+    "revision": "47009bb72e3a16741577",
+    "url": "/firegram/static/js/2.28e6e280.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/firegram/static/js/2.28e6e280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f29eb190bb1ae8254c61",
+    "url": "/firegram/static/js/main.6106662f.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
